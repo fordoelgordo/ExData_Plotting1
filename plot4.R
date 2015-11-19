683058f519fc9ca41bf6ff0112a24a40185b2dbb
fordoelgordo <fst.john@carolinacat.com>
2015-11-19 14:53:58 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -14,11 +14,12 @@ plot(data$Time, data$Sub_metering_1, pch = '', ylab = "Energy sub metering", xla
 lines(data$Time, data$Sub_metering_1)
 lines(data$Time, data$Sub_metering_2, col = "red")
 lines(data$Time, data$Sub_metering_3, col = "blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1,1), col=c("black","red","blue"), cex = .75, bty = "n")
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1,1), col=c("black","red","blue"),  bty = "n")
 plot(data$Time, data$Voltage, pch = '', xlab = "datetime", ylab = "Voltage")
 lines(data$Time, data$Voltage)
 plot(data$Time, data$Global_reactive_power, pch = '', xlab = "datetime", ylab = "Global_reactive_power")
 lines(data$Time, data$Global_reactive_power)
 
 #Save plots to a png file
-
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
+dev.off()
