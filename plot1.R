385cb22d63ea24814a8f1d1a73e2fb279587a7de
fordoelgordo <fst.john@carolinacat.com>
2015-11-19 13:38:29 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,8 @@
 #The following code assumes that you have your home directory set to the location of the dataset
 
-#Read in and subset the data set we are working with
+#Read in dataset, transform Date and Time variables and subset the dataset to dates between 2/1/2007 and 2/2/2007
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data <- transform(data, Time = strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M"))
 data <- transform(data, Date = as.Date(data$Date, "%d/%m/%Y"))
 data <- subset(data, data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 
